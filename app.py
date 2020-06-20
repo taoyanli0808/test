1fd49a3996967ce1715aab11c694b1228bd0bc74
李成龙 <lichenglong004@ke.com>
2020-06-20 17:06:08 +0800

    增加接口验签

--- app.py
+++ app.py
@@ -28,24 +28,39 @@ config = {
 }
 
 
-def execute_sql(sql):
+def _is_search(sql):
     """
+    # 判断SQL语句是否为查询语句。
     :param sql:
     :return:
     """
+    return 'select' in sql
+
+
+def execute_sql(sql):
+    """
+    # 执行SQL代码，返回执行结果。
+    :param sql:
+    :return:
+    """
+    result = None
     db = pymysql.connect(**config)
-    cursor = db.cursor()
+    cursor = db.cursor(pymysql.cursors.DictCursor)
     try:
+        print(sql)
         cursor.execute(sql)
         db.commit()
-        id = cursor.lastrowid
+        if _is_search(sql):
+            result = cursor.fetchall()
+        else:
+            result = cursor.lastrowid
     except pymysql.err.InternalError as error:
         print(error)
         db.rollback()
     finally:
         cursor.close()
     db.close()
-    return id
+    return result
 
 
 def sign(data):
@@ -54,7 +69,7 @@ def sign(data):
     :return:
     """
     # 提取请求参数里的签名，对请求进行相同方式排序。
-    signature = data.pop('sign', None)
+    signature = data.pop('signature', None)
     data.setdefault('secret', SECRET)
     keys = sorted(data.keys())
     source = ''.join([key+str(data[key]) for key in keys])
@@ -67,6 +82,7 @@ def sign(data):
     timestamp = data.pop('timestamp')
     now = time.time()
     if math.fabs(now - timestamp) > 60:
+        print("请求时间异常，请同步系统时间！")
         return False
 
     # 使用md5进行签名计算，如果指纹相同则认为验签通过。
@@ -76,43 +92,90 @@ def sign(data):
     return fingerprnt == signature
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
 @app.route('/api/v1/book/create', methods=['POST'])
 def api_v1_book_create():
     data = request.get_json()
-    if sign(data):
-        keys = ','.join(data.keys())
-        values = "'" + "','".join(list(map(str, data.values()))) + "'"
-        sql = "insert into book.book({0}) values({1})".format(keys, values)
-        print(sql)
-        id = execute_sql(sql)
-        return jsonify({
-            'status': 0,
-            'message': 'ok',
-            'data': { 'id': id }
-        })
-    else:
+
+    if not sign(data):
         return jsonify({
             'status': 400,
-            'message': 'signature verification failed!',
+            'message': '接口验签失败，请检查请求参数！',
             'data': {}
         })
 
+    keys = ','.join(data.keys())
+    values = "'" + "','".join(list(map(str, data.values()))) + "'"
+    sql = "insert into book.book({0}) values({1})".format(keys, values)
+    id = execute_sql(sql)
+
+    return jsonify({
+        'status': 0,
+        'message': 'ok',
+        'data': { 'id': id }
+    })
+
 
 @app.route('/api/v1/book/delete', methods=['DELETE'])
-def api_v1_book_delete(): pass
+def api_v1_book_delete():
+    data = request.get_json()
+
+    if 'id' not in data or not data['id']:
+        return jsonify({
+            'status': 400,
+            'message': '缺少需要删除的数据ID',
+            'data': {}
+        })
+
+    sql = "update book.book set deleted=1 where id='{}'".format(data['id'])
+    result = execute_sql(sql)
+
+    return jsonify({
+        'status': 0,
+        'message': '成功删除数据！',
+        'data': result
+    })
 
 
 @app.route('/api/v1/book/update', methods=['PUT'])
-def api_v1_book_update(): pass
+def api_v1_book_update():
+    data = request.get_json()
+
+    if 'id' not in data or not data['id']:
+        return jsonify({
+            'status': 400,
+            'message': '缺少需要删除的数据ID',
+            'data': {}
+        })
+
+    _id = data.pop('id')
+    content = ",".join([key + "=" + str(val) for key, val in data.items()])
+
+    sql = "update book.book set {} where id='{}'".format(content, _id)
+    result = execute_sql(sql)
+
+    return jsonify({
+        'status': 0,
+        'message': '成功更新数据！',
+        'data': result
+    })
 
 
 @app.route('/api/v1/book/search', methods=['GET'])
-def api_v1_book_search(): pass
+def api_v1_book_search():
+    data = request.values.to_dict()
+    # 如果请求不包含翻页参数，默认页码为0，默认返回10条数据。
+    page = data['page'] if 'page' in data and data['page'] else 0
+    limit = data['limit'] if 'limit' in data and data['limit'] else 10
+
+    # 执行SQL语句查询数据库中的图书数据。
+    sql = "select * from book.book where deleted=0 limit {},{}".format(page, limit)
+    result = execute_sql(sql)
+
+    return jsonify({
+        'status': 0,
+        'message': 'ok',
+        'data': result
+    })
 
 
 if __name__ == '__main__':
